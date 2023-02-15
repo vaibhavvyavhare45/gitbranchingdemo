16e165a8eec759dee1e96886837f6d27ba685e83
vaibhav vyavhare <113101723+vaibhavvyavhare45@users.noreply.github.com>
2023-02-15 18:44:10 +0530

    dev4 commit

--- gitbranchingdemo/src/main/java/com/prowings/Substraction.java
+++ gitbranchingdemo/src/main/java/com/prowings/Substraction.java
@@ -6,15 +6,19 @@ public class Substraction {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int a,b,c=0;
+		//dev3
 		//read element to the  for next method  to the scanner
 		a=sc.nextInt();
 		//add first print statement
 		b=sc.nextInt();
+		//dev3
 		//add second print statement
 		c=a-b;
 		System.out.println("substraction is: "+c);
 		//substraction is  success
 		//lookin for another method
+		
+		//dev3
 	}
 
 }
