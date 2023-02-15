25e8ff02bf46d78de6d82458c591092f7855a150
vaibhav vyavhare <113101723+vaibhavvyavhare45@users.noreply.github.com>
2023-02-15 18:31:33 +0530

    dev3 commit

--- gitbranchingdemo/src/main/java/com/prowings/Substraction.java
+++ gitbranchingdemo/src/main/java/com/prowings/Substraction.java
@@ -7,7 +7,9 @@ public class Substraction {
 		Scanner sc=new Scanner(System.in);
 		int a,b,c=0;
 		a=sc.nextInt();
+		//add first print statement
 		b=sc.nextInt();
+		//add second print statement
 		c=a-b;
 		System.out.println("substraction is: "+c);
 		//substraction is  success
