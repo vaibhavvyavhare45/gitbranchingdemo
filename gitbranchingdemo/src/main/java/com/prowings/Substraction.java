a5052cc1f1ccd1375c7187b859652fc5b85e85e3
vaibhav <vaibhavvyavhare45@gmail.com>
2023-02-15 18:02:57 +0530

    first commit

---
+++ gitbranchingdemo/src/main/java/com/prowings/Substraction.java
@@ -0,0 +1,15 @@
+package com.prowings;
+
+import java.util.Scanner;
+
+public class Substraction {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a,b,c=0;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a-b;
+		System.out.println("substraction is: "+c);
+	}
+
+}
