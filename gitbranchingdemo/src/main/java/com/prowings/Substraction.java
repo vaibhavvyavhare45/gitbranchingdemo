d063b34aed403e72fbab69e3429e77157c208d64
vaibhav <vaibhavvyavhare45@gmail.com>
2023-02-15 18:36:20 +0530

    second commit

--- gitbranchingdemo/src/main/java/com/prowings/Substraction.java
+++ gitbranchingdemo/src/main/java/com/prowings/Substraction.java
@@ -6,6 +6,7 @@ public class Substraction {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int a,b,c=0;
+		//read element to the  for next method  to the scanner
 		a=sc.nextInt();
 		//add first print statement
 		b=sc.nextInt();
